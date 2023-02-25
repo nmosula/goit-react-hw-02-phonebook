8325bebe2bdae9b5fd68c3cdcdbf3efeca05a805
Nazar Mosula <nmosula@gmail.com>
2023-02-25 18:39:51 +0200

    Updated UserForm

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -6,7 +6,10 @@ import * as Yup from 'yup';
 
 const UserSchema = Yup.object().shape({
     name: Yup.string()
-        .matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$)/)
+        .matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/)
+        .required('Required'),
+    number: Yup.string()
+        .matches(/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/)
         .required('Required')
 });
 
